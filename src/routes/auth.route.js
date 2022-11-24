8e9d672e66eec45d5136cfe3227779a7d5350a31
Quang Hiệp <quanghiep03198@gmail.com>
2022-11-24 08:25:59 +0700

    auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,13 +1,13 @@
 import express from "express";
-import * as _Account from "../controllers/auth.controller";
+import * as _Auth from "../controllers/auth.controller";
 import * as _User from "../controllers/user.controller";
-
+import { checkAuth } from "../middlewares/authorization.middleware";
 const router = express.Router();
 
-router.post("/register", _Account.register);
-router.post("/login", _Account.login);
-router.get("/account/activate/:token", _Account.activateAccount);
-router.get("/user", _User.get);
-router.patch("/user", _User.update);
-
+router.post("/register", _Auth.register);
+router.post("/login", _Auth.login);
+router.get("/account/activate/:token", _Auth.activateAccount);
+router.get("/user", checkAuth, _User.get);
+router.patch("/user", checkAuth, _User.update);
+router.post("/refresh-token", _Auth.refreshToken);
 export default router;
